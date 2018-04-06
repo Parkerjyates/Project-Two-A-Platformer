34ed37a8d922f37cd78707e65125a47bf1fea269
Parkerjyates <37162028+Parkerjyates@users.noreply.github.com>
2018-04-05 22:05:18 -0400

    Add files via upload

---
+++ Project Two A Platformer/code/game_levels.js
@@ -0,0 +1,99 @@
+var GAME_LEVELS = [
+
+  
+  
+  
+  
+ 
+  ["                                                                      x            ",
+   "                                                                      x            ",
+   "                                                                      x            ",
+   "                     fof            fof                               x            ",
+   "              f     xxxxxxxxxxxxxxxxxxxx                              x            ",
+   "              f                        x                              x            ",
+   "              f x                     ox                         fff  x            ",
+   "         x    xxx                     xx                         fof  x            ",
+   "              x                        x                         f f  x            ",
+   " x            x                        x                              x             ",
+   "              x                        x                ooo       r   x             ",
+   "     x        x      p                xx!!!!xx!!!!xxxxxxxxxxxxx xxxxxxx           ",
+   "              x                        x!!!!!!!!!!x             x                  ",
+   "              f                        x!!!!!!!!!!x o           x                  ",
+   "              f                        xxxvxxxxvxxxxxx          x                  ",
+   "       x      f             xxxx                                x                  ",
+   "                            x!!x                                x                  ",
+   "                            x!!x                        x       x                  ",
+   " x            f             x!!x                               xx                  ",
+   "              f       xxxx!!x!!xx                               x                  ",
+   "              f       x!!x!!x!!x                                x                  ",
+   "   @   o      x=      x!!x!!x!!x   =           =            =   x                  ",
+   " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+   "                                                                                  "],
+   
+   
+   
+   
+   
+  ["                                                                                                               ",
+   "                                                                                                               ",
+   "                                                                                                               ",
+   "                                                                                                               ",
+   "                                                                                                               ",
+   "                                                                                                               ",
+   "                                                                                                               ",
+   "xxxxxxxxxxx                                                                 xx  ",
+   "x         x                                                   xxx            x  ",
+   "x         x                                                     x            x  ",
+   "x         xxxxxxxxxxxxxxxxxxxxxx                    x           x            x  ",
+   "x          |     |     |       x                                xx           x  ",
+   "x                              x                 x!!!!!!!!!!!!!!x            x  ",
+   "x                              x                 x!!!!!!!!!!!!!!x            x  ",
+   "x             |     |          x  o              x!!!!!!!!!!!!!!x            x  ",
+   "x         xxxxxxxxxxxxxxx      xxxxxxxxxxxxxxxxxxxxvxxxxxvxxxxvxx    x       x  ",
+   "x         x             x                                                    x  ",
+   "x   @     x             x                                                   ox  ",
+   "xxxxxxxxxxx             x                                                   xx  ",
+   "                        x                                           x!!!!!!!!x  ",
+   "                        x     f  o  f  o   f  o  f  o  f  o  f  o   x!!!!!!!!x  ",
+   "                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  "],
+   
+   
+   
+   
+   
+  ["                                                                          ",
+   "                                                                          ",
+   "                                                                          ",
+   "                                                                          ",
+   "                                                                          ",
+   "                                                       o                  ",
+   "                                                    f  x                  ",
+   "                                             f  x                         ",
+   "                                     f   x                                ",
+   "                              f  x                                        ",
+   "                      f  x                                                ",
+   "  @              x                                          r          o  ",
+   "xxxxxxxxxx!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+   "xxxxxxxxxxxx!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+   "xxxxxxxxxxxxxxx!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"],
+   
+   
+   
+   
+  ["                                                                    ",
+   "       x       x  xxxxx  xx       x  xx       x  xxxx xxxxx         ",
+   "       x       x    x    x x      x  x x      x  x    x   x         ",
+   "       x       x    x    x  x     x  x  x     x  x    x   x         ",
+   "       x       x    x    x   x    x  x   x    x  x    xxxxx         ",
+   "       x   x   x    x    x    x   x  x    x   x  xxx  x    x        ",
+   "       x  x x  x    x    x     x  x  x     x  x  x    x     x       ",
+   "       x x   x x    x    x      x x  x      x x  x    x      x      ",
+   "       xx     xx  xxxxx  x       xx  x       xx  xxxx x       x     ",
+   "                                                                    ",
+   "      @                                                             ",
+   "                                                                    ",
+   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",]
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
